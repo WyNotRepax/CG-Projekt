4107312e1dbce6edee76400e2cff7d97305caa22
pueshuan <johann.n@hotmail.de>
2021-03-08 16:15:47 +0100

    Diff,Amb und Spec Color zu Gameshader

--- CG-Project/BowlingGame/src/shader/GameShader.cpp
+++ CG-Project/BowlingGame/src/shader/GameShader.cpp
@@ -27,6 +27,21 @@ void GameShader::activate()
 	}
 	mEyePosChanged = false;
 
+	if (mDiffColChanged) {
+		setUniformVec(mDiffColLoc, mDiffCol);
+	}
+	mDiffColChanged = false;
+
+	if (mSpecColChanged) {
+		setUniformVec(mSpecColLoc, mSpecCol);
+	}
+	mSpecColChanged = false;
+
+	if (mAmbColChanged) {
+		setUniformVec(mAmbColLoc, mAmbCol);
+	}
+	mAmbColChanged = false;
+
 	setUniformId(mDifftexLoc, 0);
 	LOG_CALL(glActiveTexture, GL_TEXTURE0);
 	LOG_CALL(glBindTexture, GL_TEXTURE_2D, mDiffTexId);
@@ -55,6 +70,24 @@ void GameShader::setEyePos(const Vector& eyePos)
 	mEyePosChanged = true;
 }
 
+void GameShader::setDiffCol(const Vector& diffCol)
+{
+	mDiffCol = diffCol;
+	mDiffColChanged = true;
+}
+
+void GameShader::setSpecCol(const Vector& specCol)
+{
+	mSpecCol = specCol;
+	mSpecColChanged = true;
+}
+
+void GameShader::setAmbCol(const Vector& ambCol)
+{
+	mAmbCol = ambCol;
+	mAmbColChanged = true;
+}
+
 GameShader::GameShader() :
 	Shader(GAME_SHADER_VS_PATH, GAME_SHADER_FS_PATH),
 	mViewProjChanged(true),
@@ -66,10 +99,22 @@ GameShader::GameShader() :
 	mModelChanged(true),
 	mEyePos(Vector(0, 0, 0)),
 	mEyePosChanged(true),
-	mEyePosLoc(0)
+	mEyePosLoc(0),
+	mDiffCol(Vector(0, 0, 0)),
+	mDiffColChanged(true),
+	mDiffColLoc(0),
+	mSpecCol(Vector(0, 0, 0)),
+	mSpecColChanged(true),
+	mSpecColLoc(0),
+	mAmbCol(Vector(0, 0, 0)),
+	mAmbColChanged(true),
+	mAmbColLoc(0)
 {
 	mModelViewProjLoc = Shader::getUniformLocation(GAME_SHADER_MODELVIEWPROJ_UNIFORM_NAME);
 	mDifftexLoc = Shader::getUniformLocation(GAME_SHADER_DIFFTEX_UNIFORM_NAME);
 	mModelLoc = Shader::getUniformLocation(GAME_SHADER_MODEL_UNIFORM_NAME);
 	mEyePosLoc = Shader::getUniformLocation(GAME_SHADER_EYEPOS_UNIFORM_NAME);
+	mDiffColLoc = Shader::getUniformLocation(GAME_SHADER_DIFFCOL_UNIFORM_NAME);
+	mSpecColLoc = Shader::getUniformLocation(GAME_SHADER_SPECCOL_UNIFORM_NAME);
+	mAmbColLoc = Shader::getUniformLocation(GAME_SHADER_AMBCOL_UNIFORM_NAME);
 }
--- CG-Project/BowlingGame/src/shader/GameShader.h
+++ CG-Project/BowlingGame/src/shader/GameShader.h
@@ -6,6 +6,9 @@
 #define GAME_SHADER_DIFFTEX_UNIFORM_NAME "DiffTex"
 #define GAME_SHADER_MODEL_UNIFORM_NAME "Model"
 #define GAME_SHADER_EYEPOS_UNIFORM_NAME "EyePos"
+#define GAME_SHADER_DIFFCOL_UNIFORM_NAME "DiffuseColor"
+#define GAME_SHADER_SPECCOL_UNIFORM_NAME "SpecularColor"
+#define GAME_SHADER_AMBCOL_UNIFORM_NAME "AmbientColor"
 #define GAME_SHADER_FS_PATH SHADER_DIR"/fsmodel.glsl"
 #define GAME_SHADER_VS_PATH SHADER_DIR"/vsmodel.glsl"
 
@@ -32,6 +35,18 @@ private:
 	GLint mEyePosLoc;
 	bool mEyePosChanged;
 
+	Vector mDiffCol;
+	GLint mDiffColLoc;
+	bool mDiffColChanged;
+
+	Vector mSpecCol;
+	GLint mSpecColLoc;
+	bool mSpecColChanged;
+
+	Vector mAmbCol;
+	GLint mAmbColLoc;
+	bool mAmbColChanged;
+
 	//Member Funcions
 public:
 	GameShader(const GameShader& other) = delete;
@@ -43,6 +58,9 @@ public:
 	void setModel(const Matrix& matrix);
 	void setDiffTex(GLuint texId);
 	void setEyePos(const Vector& eyePos);
+	void setDiffCol(const Vector& diffCol);
+	void setSpecCol(const Vector& specCol);
+	void setAmbCol(const Vector& ambCol);
 protected:
 	GameShader();
 };
